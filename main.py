3db588b9cd56589c8d224ca2127c3a1b7d889381
dirkarnez <smalldirkalex@gmail.com>
2024-02-12 17:11:23 +0800

    - add resize

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from moviepy.editor import *
+import moviepy.video.fx.all as vfx
 
 filename = "mov_bbb"
-video = VideoFileClip(f"{filename}.mp4").set_position(("center","bottom")) #.subclip(0,2)
+video = (VideoFileClip(f"{filename}.mp4")
+            .fx(vfx.resize, width=1080)
+            .set_position(("center","bottom")))#.subclip(0,2))
 
 # Make the text. Many more options are available.
-txt_clip = ( TextClip("My Holidays 2013", fontsize=15, color='white')
-            .set_position(("center","top"))
+txt_clip = ( TextClip(txt='line 1' + os.linesep + 'line 2', fontsize=50, color='white', font="IMPACT", align="west")
+            .set_position((40, 40))
             .set_duration(2)  )
 
-result = CompositeVideoClip([video, txt_clip], size=(video.w, video.h + 60), bg_color=(12, 34, 56)) # Overlay text on video
+result = CompositeVideoClip([video, txt_clip], size=(video.w, video.h + 200), bg_color=(12, 34, 56)) # Overlay text on video
 
 result.write_videofile(f"{filename}_edited.mp4") # Many options...
